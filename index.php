ea5bc1a8a77076f2cc6b7954bb94382325829102
Nicholas Caswell <nwcaswell@gmail.com>
2019-04-04 16:51:20 +0000

    final changes 4/4

--- index.php
+++ index.php
@@ -296,30 +296,23 @@ if(mail($to,$subject,$message,$headers) == true){
   <?php endwhile; ?>
 </table>
 <script>
-  //var server = require('../server.js');
-  //server.getdbinfo();
     
   function openCity(evt, cityName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
 function myFunction() {
-  // Declare variables 
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
@@ -355,9 +348,7 @@ function myFunction() {
       filt0 = 0;
       break;
   }
-  //console.log(filt0);
 
-  // Loop through all table rows, and hide those who don't match the search query
   for (i = 1; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("th")[filt0];//[0];
     if (td) {
